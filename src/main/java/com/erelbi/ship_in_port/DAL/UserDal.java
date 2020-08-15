a54c5c46d2c7479037ad2ac2925540d1aa7c422e
emrecirakoglu <emrecirakoglux@gmail.com>
2020-08-15 10:26:29 +0300

    user  update isleminde mail validation yapıldı.

--- src/main/java/com/erelbi/ship_in_port/DAL/UserDal.java
+++ src/main/java/com/erelbi/ship_in_port/DAL/UserDal.java
@@ -35,16 +35,10 @@ public class UserDal {
 
     public void saveUser(User user){
 
-        // Check user register before. Not allow user to create with same email address. 
-        String userMail = user.getEmail();
-        if (userRepository.existsByEmail(userMail)) {
-            throw new EntityExistsException("User already created before!");
-        }
-
-        if (user.getId() != null){
-            this.updateUser(user);
-        } else {
+        if (user.getId() == null){
             userRepository.save(user);
+        } else {
+            this.updateUser(user);
         }
         
     }
@@ -60,19 +54,41 @@ public class UserDal {
     }
 
     public void updateUser(User user){
-        List<Port> ports = user.getVisitedPorts();
-        List<Port> portsSetted = new ArrayList<>();
 
-        if (ports.isEmpty()) {
+        if ( this.checkEmailValidation(user) ){
+            List<Port> ports = user.getVisitedPorts();
+            if (!ports.isEmpty()) {
+                List<Port> portsSetted = new ArrayList<>();
+                for (Port port : ports) {
+                    portsSetted.add(this.portRepository.getOne(port.getId())) ;
+                }
+                user.setVisitedPorts(portsSetted);
+            }
             this.userRepository.save(user);
         } else {
-            for (Port port : ports) {
-                portsSetted.add(this.portRepository.getOne(port.getId())) ;
-            }
+            throw new EntityExistsException("This e-mail address is registered in the system.");
         }
 
-        user.setVisitedPorts(portsSetted);
-        this.userRepository.save(user);
+    }
 
+    public Boolean checkUserExists(User user){
+        // Check user register before. Not allow user to create with same email address. 
+        String userMail = user.getEmail();
+        if (userRepository.existsByEmail(userMail)) {
+            throw new EntityExistsException("User already created before!");
+        } else {
+            return false;
+        }
+    }
+
+    public Boolean checkEmailValidation(User user){   
+        // When user attempt to update his mail, check new email using from anohter user.     
+        List<User> users = this.userRepository.findByEmail(user.getEmail());
+        for (User user2 : users) {
+            if(user.getId() != user2.getId()){
+                return false;
+            }
+        }
+        return true;
     }
 }
